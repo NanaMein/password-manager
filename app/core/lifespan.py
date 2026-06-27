fa918389e0c670202671802bc1c4624be744f5f2
Nana Mein <alfeoephraimquintay@gmail.com>
2026-06-27 23:01:07 +0800

    Feature: Added Startup status to see and understand if user is new or old. it also creates folders for new users so that a file is prepared to encrypt and decrypt.

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -2,6 +2,75 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from cachetools import TTLCache
 from datetime import timedelta
+from pydantic import BaseModel, Field
+from app.core.config import get_settings_instance
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from pathlib import Path
+
+
+
+class StartupStatus(BaseModel):
+    timestamp: datetime = Field(default_factory=lambda: datetime.now(ZoneInfo("Asia/Manila")))
+    working_directory: str = "pending"
+    vault_directory: str = "pending"
+
+    initial_user_status: str = "pending"
+
+
+
+class StartupStateManagement:
+    def __init__(self, startup_status: StartupStatus):
+        self._startup_status = startup_status
+        self.settings = get_settings_instance()
+
+    @property
+    def generate_updated_status(self):
+        self.working_directory_status_update()
+        self.vault_directory_status_update()
+        self.user_status_update()
+        return self._startup_status
+
+    @staticmethod
+    def _create_new_directory(file: Path):
+        if not file.is_dir():
+            file.mkdir(parents=True, exist_ok=True)
+            test_file = file / ".write_test"
+            test_file.touch()
+            test_file.unlink()
+            return True
+        return False
+
+    def working_directory_status_update(self):
+        is_created = self._create_new_directory(file=self.settings.working_dir)
+        if is_created:
+            self._startup_status.working_directory = "NEW"
+
+        else:
+            self._startup_status.working_directory = "OLD"
+
+        return self
+
+    def vault_directory_status_update(self):
+        is_created = self._create_new_directory(file=self.settings.secret_vault_storage)
+        if is_created:
+            self._startup_status.vault_directory = "NEW"
+
+        else:
+            self._startup_status.vault_directory = "OLD"
+
+        return self
+
+    def user_status_update(self):
+        if self._startup_status.working_directory == "OLD" and self._startup_status.vault_directory == "OLD":
+            self._startup_status.initial_user_status = "EXISTING_USER"
+
+        elif self._startup_status.working_directory == "NEW" and self._startup_status.vault_directory == "OLD":
+            self._startup_status.initial_user_status = "EXISTING_USER"
+
+        else:
+            self._startup_status.initial_user_status = "NEW_USER"
+        return self
 
 
 def time_to_live_cache(
@@ -33,6 +102,13 @@ def time_to_live_cache(
 async def lifespan(app: FastAPI):
     app.state.access_cache = time_to_live_cache(minutes=5)
     app.state.refresh_cache = time_to_live_cache(hours=2)
+
+
+    startup_status = StartupStatus()
+    updated_status = StartupStateManagement(startup_status)
+    app.state.startup_status = updated_status.generate_updated_status
+
+
     try:
         yield
     finally:

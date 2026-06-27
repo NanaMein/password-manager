13520969432f4314d7f974bc6a51685c72137528
Nana Mein <alfeoephraimquintay@gmail.com>
2026-06-28 01:20:57 +0800

    Chore: Simplified and did distribution of responsibility for function unlock_files. Also return boolean rather than dict status. Also with the distribution of function, the other function can be used to decrypt without unloading the files.

--- app/services/unlocking_service.py
+++ app/services/unlocking_service.py
@@ -8,7 +8,48 @@ class UnlockingFileService:
         self.settings = get_settings_instance()
         self.derive_key = key_derivation_service
 
+    def decrypted_fernet(self, passphrase: str | bytes):
+        if isinstance(passphrase, str):
+            passphrase = bytes(passphrase, "utf-8")
+        try:
+            salt, token = self.derive_key.get_salt_and_token(self.settings.master_vault_file)
+            key = self.derive_key.get_key(passphrase, salt)
+            decrypted = Fernet(key).decrypt(token)
+            return decrypted
+        except InvalidToken as iex:
+            raise iex
+        except Exception as ex:
+            raise ex
+
+    def check_vault_validity(self, passphrase: bytes | str):
+        if isinstance(passphrase, str):
+            passphrase = bytes(passphrase, "utf-8")
+
+        try:
+            self.decrypted_fernet(passphrase)
+            return True
+        except (InvalidToken, Exception) as iex:
+            return False
+
     def unlock_files(self, passphrase: bytes | str):
+        try:
+            decrypted = self.decrypted_fernet(passphrase)
+            parts = decrypted.split(self.settings.separator)
+            count = 0
+            for i in range(1, len(parts), 2):
+                name = parts[i].decode()
+                content = parts[i + 1]
+
+                file_path = self.settings.working_dir / name
+                file_path.parent.mkdir(parents=True, exist_ok=True)
+                file_path.write_bytes(content)
+                count += 1
+            return True
+        except (InvalidToken, Exception) as e:
+            return False
+
+
+    def unlock_files_old(self, passphrase: bytes | str):
         file_status = {}
         if isinstance(passphrase, str):
             passphrase = bytes(passphrase, "utf-8")
